8601f95f94c7f02bdb966ea18d677019fb5afbec
Brayan Hernandez <brayanhernandezfurnieles@gmail.com>
2024-02-20 17:29:53 -0500

    Initial commit

---
+++ src/main/java/com/distributor/warehouse/DistributorWarehouseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distributor.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributorWarehouseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributorWarehouseServiceApplication.class, args);
+    }
+
+}
